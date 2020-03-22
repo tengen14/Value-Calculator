1211e0292cdeb4170e3ad87bbfa79bacb8008ff6
Thane Engen <thaneengen@gmail.com>
2020-03-21 18:02:48 -0600

    console out equation registration on input change

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const result = document.querySelector("#result");
 const inputValues = document.getElementsByClassName("input-value");
 
+// equation data
+// way to dry up this code?
 
-for (let variable of inputValues) {
-    console.log(variable.value);
+for (var i = 0; i < inputValues.length; i++) {
+  inputValues[i].addEventListener("change", function() {
+    let employees = parseInt(inputValues[0].value);
+    let costPerHour = parseInt(inputValues[1].value);
+    let hoursSaved = parseInt(inputValues[2].value);
+    
+    let newResult = employees * (4 * costPerHour) * hoursSaved;
+    console.log(newResult);
+  });
 }
