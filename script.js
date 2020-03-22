b2438756a64688cad1231be8b694d813b8a6dcd2
Thane Engen <thaneengen@gmail.com>
2020-03-21 18:15:55 -0600

    update DOM on variable change, change result of equation

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const result = document.querySelector("#result");
-const inputValues = document.getElementsByClassName("input-value");
+let result = document.querySelector("#result");
+let inputValues = document.getElementsByClassName("input-value");
 
-// equation data
-// way to dry up this code?
+console.log(result);
 
 for (var i = 0; i < inputValues.length; i++) {
   inputValues[i].addEventListener("change", function() {
     let employees = parseInt(inputValues[0].value);
     let costPerHour = parseInt(inputValues[1].value);
     let hoursSaved = parseInt(inputValues[2].value);
-    
+
     let newResult = employees * (4 * costPerHour) * hoursSaved;
+    newResult = newResult.toString();
     console.log(newResult);
+
+    result.innerHTML=newResult;
+    console.log(result);
   });
 }
